67019965281a0a51c547946fc0e778aef24d7371
aprosvirin <aprosvirin@ugcs.com>
2021-08-12 18:02:03 +0300

    Implemented native route upload

---
+++ app/src/main/java/com/example/ugcssample/drone/DroneBridge.kt
@@ -0,0 +1,21 @@
+package com.example.ugcssample.drone
+
+import android.content.Context
+import com.example.ugcssample.utils.PermissionCheckResult
+import dji.sdk.products.Aircraft
+import java.io.File
+
+interface DroneBridge {
+    fun submitLocationInit()
+    fun submitSdkInit()
+    fun onUsbAccessoryAttached()
+    val permissionCheckResult: PermissionCheckResult?
+    val aircraftInstance: Aircraft?
+    fun startModelSimulator()
+    fun onDestroy()
+    fun takeOff()
+    fun land(useKeyInterface: Boolean)
+    fun uploadDemoMission()
+    fun startMission()
+    fun uploadMission(nativeRoute : File)
+}
